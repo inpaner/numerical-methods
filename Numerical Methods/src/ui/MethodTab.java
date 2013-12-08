dd410504921178d865fc3aee59166a655889b9a3
Ivan Paner <ivan.paner@gmail.com>
2013-12-08 18:29:34 +0800

    Begun MethodTab

---
+++ Numerical Methods/src/ui/MethodTab.java
@@ -0,0 +1,61 @@
+package ui;
+
+import java.text.DecimalFormat;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import net.miginfocom.swing.MigLayout;
+import methods.Method;
+import methods.Polynomial;
+import methods.RegulaFalsi;
+
+public class MethodTab extends JPanel {
+    private Method method;
+    private JLabel iterationsValue;
+    private JTable iterationsTable;
+    private JLabel fOfXValue;
+    
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        MethodTab tab = new MethodTab();
+        frame.setPanel(tab);
+        Polynomial poly = new Polynomial(1, 0, -78.8);
+        RegulaFalsi falsi = new RegulaFalsi(poly, 6, 12);
+        Method method = new RegulaFalsi(poly, 0, 10);
+        tab.updateMethod(falsi);
+    }
+    
+    MethodTab() {
+        JLabel iterationsLabel = new JLabel("Iterations: ");
+        iterationsValue = new JLabel("");
+        
+        JLabel fOfXLabel = new JLabel("Final f(x): ");
+        fOfXValue = new JLabel("");
+        
+        // table
+        iterationsTable = new JTable();
+        JScrollPane tablePane = new JScrollPane(iterationsTable);
+        
+        setLayout(new MigLayout("wrap 2"));
+        add(iterationsLabel);
+        add(iterationsValue);
+        add(fOfXLabel);
+        add(fOfXValue);
+        add(tablePane, "span, split");
+    }
+    
+    void updateMethod(Method method) {
+        this.method = method;
+        iterationsValue.setText( String.valueOf(method.getTotalIterations()) );
+        
+        DecimalFormat df = new DecimalFormat("0.000000");
+        String finalY = df.format(method.getFinalY());
+        fOfXValue.setText( finalY );
+        
+        MethodTableModel methodModel = new MethodTableModel( method );
+        iterationsTable.setModel( methodModel );
+    }
+}
