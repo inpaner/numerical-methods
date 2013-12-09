4eabb3453fbeb75b3d50af33301bfb9ba4343cce
Ivan Paner <ivan.paner@gmail.com>
2013-12-09 19:19:59 +0800

    Graph now extends to the limits of the line

--- Numerical Methods/src/ui/GraphPanel.java
+++ Numerical Methods/src/ui/GraphPanel.java
@@ -18,8 +18,10 @@ import methods.Polynomial;
 @SuppressWarnings("serial")
 public class GraphPanel extends JPanel {
     private final int GRAPH_STEPS = 100;
+    private Polynomial polynomial;
     private XYSeriesCollection dataset;
-    private XYSeries polynomialSeries;
+    private double inputLowerBound;
+    private double inputUpperBound;
 
     GraphPanel() {
         dataset = new XYSeriesCollection();
@@ -38,25 +40,48 @@ public class GraphPanel extends JPanel {
     
     void updatePolynomial(Polynomial polynomial, double lowerBound, double upperBound) {
         assert lowerBound <= upperBound : "lower bound > upper bound";
+        this.polynomial = polynomial;
+        inputLowerBound = lowerBound;
+        inputUpperBound = upperBound;
         
         dataset.removeAllSeries();
-        polynomialSeries = new XYSeries(polynomial.toString());
+        XYSeries polynomialSeries = createPolynomialSeries(polynomial, lowerBound, upperBound);
+        
+        dataset.addSeries(polynomialSeries);
+    }
+    
+    private XYSeries createPolynomialSeries(Polynomial polynomial, double lowerBound, double upperBound) {
+        XYSeries result = new XYSeries(polynomial.toString());
         
         double range = upperBound - lowerBound;
         double stepSize = range / GRAPH_STEPS; 
         
         for (double x = lowerBound; x <= upperBound; x += stepSize) {
             double y = polynomial.evaluate(x);
-            polynomialSeries.add(x, y);
+            result.add(x, y);
         }
         
-        dataset.addSeries(polynomialSeries);
+        return result;
     }
     
     void updateLine(Line line) {
         XYSeries lineSeries = new XYSeries("");
         lineSeries.add(line.getX0(), line.getY0());
         lineSeries.add(line.getX1(), line.getY1());
+        
+        System.out.println("inputlb: " + inputLowerBound + " ,x0: " + line.getX0());
+        
+        double chartLowerBound = inputLowerBound;
+        if (chartLowerBound > Math.min(line.getX0(), line.getX1())) {
+            chartLowerBound = Math.min(line.getX0(), line.getX1());
+        }
+        double chartUpperBound = inputUpperBound;
+        if (chartUpperBound < Math.max(line.getX0(), line.getX1())) {
+            chartUpperBound = Math.max(line.getX0(), line.getX1());
+        }
+        
+        XYSeries polynomialSeries = createPolynomialSeries(
+                polynomial, chartLowerBound, chartUpperBound);
         
         dataset.removeAllSeries();
         dataset.addSeries(polynomialSeries);
