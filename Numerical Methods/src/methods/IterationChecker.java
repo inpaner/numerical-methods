8ae83c26a959869bb4e7d3581d4e8b5e2c608d8e
Ivan Paner <ivan.paner@gmail.com>
2013-12-09 19:19:46 +0800

    Added divergence limit in case IterationChecker is disabled

--- Numerical Methods/src/methods/IterationChecker.java
+++ Numerical Methods/src/methods/IterationChecker.java
@@ -1,13 +1,17 @@
 package methods;
 
 public class IterationChecker {
-    private static int totalIterations = 20;
+    private static int totalIterations = 50;
+    private static final int divergenceLimit = 50;
     private static boolean isActive = true;
     
     public static boolean isFinishedIterating(int iterations) {
         boolean isFinished = false; 
         if (IterationChecker.isActive) {
-               isFinished = iterations >= totalIterations;  
+           isFinished = iterations >= totalIterations;  
+        }
+        else {
+            isFinished = iterations >= divergenceLimit;
         }
         return isFinished;
     }
